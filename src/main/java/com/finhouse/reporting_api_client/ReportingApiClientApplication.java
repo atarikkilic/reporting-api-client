14ea9a5d9147763eb52c13ad1753fc7c278926c5
Tarik Kilic <tarikkilic07@gmail.com>
2024-11-23 23:25:35 +0300

    Initialize project structure with Maven and Spring Boot

---
+++ src/main/java/com/finhouse/reporting_api_client/ReportingApiClientApplication.java
@@ -0,0 +1,13 @@
+package com.finhouse.reporting_api_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingApiClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingApiClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finhouse/reporting_api_client/ReportingApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finhouse.reporting_api_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
